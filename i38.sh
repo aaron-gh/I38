2e15449d9a7a83240ace3f48b038f1e9ac1902f2
Storm Dragon <stormdragon2976@gmail.com>
2023-10-15 11:58:33 -0400

    Fixed a bug with restarting remind on i3 restart. Fixed a bug with battery_alert not being loaded.

--- i38.sh
+++ i38.sh
@@ -594,7 +594,7 @@ bindsym Shift+o exec $(command -v orca) --replace, mode "default"
 # reload the configuration file
 bindsym Control+semicolon exec bash -c '$i3msg -t run_command reload && spd-say -P important -Cw "I38 Configuration reloaded."', mode "default"
 # restart i3 inplace (preserves your layout/session, can be used to upgrade i3)
-bindsym Control+Shift+semicolon exec bash -c 'pgrep remind && killall remind;$i3msg -t run_command restart && spd-say -P important -Cw "I3 restarted."', mode "default"
+bindsym Control+Shift+semicolon exec bash -c 'killall remind || true && $i3msg -t run_command restart && spd-say -P important -Cw "I3 restarted."', mode "default"
 # Run dialog with exclamation 
 bindsym Shift+exclam exec ${i3Path}/scripts/run_dialog.sh, mode "default"
 # exit i3 (logs you out of your X session)
@@ -637,7 +637,7 @@ if command -v remind &> /dev/null && command -v notify-send &> /dev/null ; then
     echo "exec_always --no-startup-id $(command -v remind) -z '-k:${HOME}/.config/i3/scripts/reminder.sh %s &' ${HOME}/.reminders < /dev/null > /dev/null 2>&1"
     touch ~/.reminders
 fi
-exec_always --no-startup-id ${i3Path}/scripts/sound.py
+echo "exec_always --no-startup-id ${i3Path}/scripts/battery_alert.sh"
 if [[ $dex -eq 0 ]]; then
     echo '# Start XDG autostart .desktop files using dex. See also'
     echo '# https://wiki.archlinux.org/index.php/XDG_Autostart'
