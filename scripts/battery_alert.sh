62a7e83baa72610f4bd8db6b6445fe75cdec259e
Aaron Hewitt <aaron.graham.hewitt@gmail.com>
2024-05-28 10:47:58 +0100

    Fixed battery alert script.

--- scripts/battery_alert.sh
+++ scripts/battery_alert.sh
@@ -1,25 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! command -v acpi &> /dev/null ; then
+if ! command -v acpi &> /dev/null; then
     exit 0
 fi
 
 while : ; do
-    acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
-        read -r status capacity
-                                                                                                                                                                          
-        if [[ "$status" == "Discharging" ]] && [[ "$capacity" -le 15 ]] && [[ "$capacity" -gt 10 ]]; then
+    acpi_output=$(acpi -b)
+    status=$(echo "$acpi_output" | awk -F'[,:%]' '{print $2}')
+    capacity=$(echo "$acpi_output" | awk -F'[,:%]' '{print $3}')
+
+    if [[ "$status" == "Discharging" ]]; then
+        if [[ "$capacity" -le 15 ]] && [[ "$capacity" -gt 10 ]]; then
             play -qV0 "|sox -n -p synth saw E2 fade 0 0.25 0.05" "|sox -n -p synth saw E2 fade 0 0.25 0.05" norm -7
             spd-say -P important "Battery $capacity percent."
-        elif [[ "$status" == "Discharging" ]] && [[ "$capacity" -le 10 ]] && [[ "$capacity" -gt 5 ]]; then
+        elif [[ "$capacity" -le 10 ]] && [[ "$capacity" -gt 5 ]]; then
             play -qV0 "|sox -n -p synth saw E2 fade 0 0.25 0.05" "|sox -n -p synth saw E2 fade 0 0.25 0.05" norm -7
             spd-say -P important "Battery $capacity percent."
-        elif [[ "$status" == "Discharging" ]] && [[ "$capacity" -lt 5 ]]; then
+        elif [[ "$capacity" -lt 5 ]]; then
             play -qV0 "|sox -np synth sq C#5 sq D#5 sq F#5 sq A5 sq C#6" remix - fade 0 5.5 5 pitch -400
             spd-say -P important "Battery $capacity percent."
         fi
-    }
-    sleep 5m
+    fi
+
+    sleep 5
 done
 
 exit 0
+
